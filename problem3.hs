fdd581bf6deb2c93e3bad1ada485658c5835eef6
codeguru <codeguru@users.sourceforge.net>
2012-10-04 07:05:16 -0600

    Import ProjectEuler module for Problem 3

--- problem3.hs
+++ problem3.hs
@@ -6,11 +6,7 @@
 
 -- Problem 3
 
-isPrime :: Integer -> Bool
-isPrime p = (divisors p) == [1, p]
-
-divisors :: Integer -> [Integer]
-divisors n = [d | d <- [1..n], n `mod` d == 0]
+import ProjectEuler
 
 main = print (head (reverse (filter isPrime (divisors n))))
 	where n = 600851475143
