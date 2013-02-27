c0700b05cc99ef037f09edcf2a32b987836b825c
codeguru <codeguru@users.sourceforge.net>
2013-02-26 23:06:37 -0700

    Problem 54: Finish

--- problem54.hs
+++ problem54.hs
@@ -7,7 +7,7 @@
 -- Problem 54
 
 import System.IO(openFile, hGetContents, hClose, IOMode(ReadMode))
-import Data.List(maximumBy, sort, sortBy)
+import Data.List(maximumBy, sort, sortBy, find)
 
 data FaceValue = Two
                | Three
@@ -38,86 +38,58 @@ data Card = Card {
 instance Show Card
          where show (Card face suit) = (show face) ++ " of " ++ (show suit)
 
-data Rank = Rank RankOrd RankInfo
-            deriving (Eq)
+data Rank  = HighCard [FaceValue]
+             -- HighCard vs where vs is a list of the face values of the
+             -- cards in descending order
+           | OnePair FaceValue [FaceValue]
+             -- OnePair v [v1,v2,v3] is a pair of vs with v1 high, v2 next,
+             -- and v3 low
+           | TwoPairs FaceValue FaceValue FaceValue
+             -- TwoPairs v1 v2 v' is a pair of v1s and a pair of v2s 
+             -- (with v1 > v2) and a v'
+           | ThreeOfAKind FaceValue [FaceValue]
+             -- ThreeOfAKind v [v1,v2] has three vs with v1 high and v2 low
+           | Straight FaceValue
+             -- Straght v has high card v
+           | Flush [FaceValue]
+             -- Flush vs where vs is a list of the face values of the cards
+             -- in descending order
+           | FullHouse FaceValue FaceValue
+             -- FullHouse v1 v2 is v1 full of v2
+           | FourOfAKind FaceValue FaceValue 
+             -- FourOfAKind v v' has four vs with v' kicker
+           | StraightFlush FaceValue
+             -- StraightFlush v has high card v
+           | RoyalFlush
+           deriving (Ord, Eq)
 
 instance Show Rank
-         where show (Rank HighCardOrd (HighCardInfo (v:vs))) =
+         where show (HighCard (v:vs)) =
                  (show v) ++ " high"
-               show (Rank OnePairOrd (OnePairInfo p (v:vs))) =
+               show (OnePair p (v:vs)) =
                  "Pair of " ++ (show p) ++ "s with " ++ (show v) ++ " high"
-               show (Rank TwoPairsOrd (TwoPairsInfo p1 p2 v)) =
+               show (TwoPairs p1 p2 v) =
                  (show p1) ++ "s and " ++ (show p2) ++ "s"
-               show (Rank ThreeOfAKindOrd (ThreeOfAKindInfo t v)) =
+               show (ThreeOfAKind t v) =
                  "Three " ++ (show t) ++ "s"
-               show (Rank StraightOrd (StraightInfo high)) =
+               show (Straight high) =
                  "Straight " ++ (show high) ++ " high"
-               show (Rank FlushOrd (FlushInfo high)) =
+               show (Flush high) =
                  "Flush " ++ (show high) ++ " high"
-               show (Rank FullHouseOrd (FullHouseInfo v1 v2)) =
+               show (FullHouse v1 v2) =
                  (show v1) ++ " full of " ++ (show v2) ++ "s"
-               show (Rank FourOfAKindOrd (ThreeOfAKindInfo t v)) =
+               show (FourOfAKind t v) =
                  "Four " ++ (show t) ++ "s"
-               show (Rank StraightFlushOrd (StraightFlushInfo high)) =
+               show (StraightFlush high) =
                  "Straight flush " ++ (show high) ++ " high"
-               show (Rank RoyalFlushOrd RoyalFlushInfo) =
+               show RoyalFlush =
                  "Royal flush"
-
-instance Ord Rank
-         where compare 
-                 (Rank HighCardOrd (HighCardInfo v1s))
-                 (Rank HighCardOrd (HighCardInfo v2s)) = compare v1s v2s
---               compare (Rank OnePairOrd (OnePairInfo p (v:vs))) =
---               compare (Rank TwoPairsOrd (TwoPairsInfo p1 p2 v)) =
---               compare (Rank ThreeOfAKindOrd (ThreeOfAKindInfo t v)) =
---               compare (Rank StraightOrd (StraightInfo high)) =
---               compare (Rank FlushOrd (FlushInfo high)) =
---               compare (Rank FullHouseOrd (FullHouseInfo v1 v2)) =
---               compare (Rank FourOfAKindOrd (ThreeOfAKindInfo t v)) =
---               compare (Rank StraightFlushOrd (StraightFlushInfo high)) =
---               compare (Rank RoyalFlushOrd (RoyalFlushInfo high)) =
-
-data RankOrd = HighCardOrd
-             | OnePairOrd
-             | TwoPairsOrd
-             | ThreeOfAKindOrd
-             | StraightOrd
-             | FlushOrd
-             | FullHouseOrd
-             | FourOfAKindOrd
-             | StraightFlushOrd
-             | RoyalFlushOrd
-             deriving (Show, Eq)
-
-data RankInfo = HighCardInfo [FaceValue]
-                -- HighCard vs where vs is a list of the face values of the
-                -- cards in descending order
-              | OnePairInfo FaceValue [FaceValue]
-                -- OnePair v [v1,v2,v3] is a pair of vs with v1 high, v2 next,
-                -- and v3 low
-              | TwoPairsInfo FaceValue FaceValue FaceValue
-                -- TwoPairs v1 v2 v' is a pair of v1s and a pair of v2s 
-                -- (with v1 > v2) and a v'
-              | ThreeOfAKindInfo FaceValue [FaceValue]
-                -- ThreeOfAKind v [v1,v2] has three vs with v1 high and v2 low
-              | StraightInfo FaceValue
-                -- Straght v has high card v
-              | FlushInfo [FaceValue]
-                -- Flush vs where vs is a list of the face values of the cards
-                -- in descending order
-              | FullHouseInfo FaceValue FaceValue
-                -- FullHouse v1 v2 is v1 full of v2
-              | FourOfAKindInfo FaceValue FaceValue 
-                -- FourOfAKind v v' has four vs with v' kicker
-              | StraightFlushInfo FaceValue
-                -- StraightFlush v has high card v
-              | RoyalFlushInfo
-              deriving (Show, Eq)
 
 deck :: [Card]
 deck = [Card face suit | 
         suit <- [Hearts, Spades, Diamonds, Clubs],
-        face <- [Ace, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King]]
+        face <- [Ace, Two, Three, Four, Five, Six, Seven, 
+                 Eight, Nine, Ten, Jack, Queen, King]]
 
 toFaceValue :: Char -> FaceValue
 toFaceValue 'A' = Ace
@@ -151,33 +123,31 @@ faceValues = map faceValue
 
 rank :: [Card] -> Rank
 rank hand
-  | isRoyalFlush hand    = Rank RoyalFlushOrd RoyalFlushInfo
-  | isStraightFlush hand = Rank
-                           StraightFlushOrd
-                           (StraightFlushInfo (maximum values))
-  | isFourOfAKind hand   = let v = 1
-                               v' = 1 
-                           in Rank FourOfAKindOrd (FourOfAKindInfo v v')
-  | isFullHouse hand     = let v = 1
-                               v' = 1
-                           in Rank FullHouseOrd (FullHouseInfo v v')
-  | isFlush hand         = Rank FlushOrd (FlushInfo (sortBy compare values))
-  | isStraight hand      = Rank StraightOrd (StraightInfo (maximum values))
-  | isThreeOfAKind hand  = let v = 1
-                               vs = 1
-                           in Rank ThreeOfAKindOrd (ThreeOfAKindInfo v vs)
-  | isTwoPairs hand      = let p1 = 1
-                               p2 = 1
-                               v' = 1
-                           in Rank TwoPairsOrd (TwoPairsInfo p1 p2 v')
-  | isOnePair hand       = let p = 1
-                               vs = 1
-                           in Rank OnePairOrd (OnePairInfo p vs)
-  | otherwise            = Rank 
-                           HighCardOrd 
-                           (HighCardInfo (sortBy compare values))
+  | isRoyalFlush hand    = RoyalFlush
+  | isStraightFlush hand = StraightFlush (maximum values)
+  | isFourOfAKind hand   = let v = findForCount 4 values
+                               v' = head (allExcept v values)
+                           in FourOfAKind v v'
+  | isFullHouse hand     = let v = findForCount 3 values
+                               v' = head (allExcept v values)
+                           in FullHouse v v'
+  | isFlush hand         = Flush (sortBy compare values)
+  | isStraight hand      = Straight (maximum values)
+  | isThreeOfAKind hand  = let v = findForCount 3 values
+                               vs = allExcept v values
+                           in ThreeOfAKind v vs
+  | isTwoPairs hand      = let p1 = findForCount 2 values
+                               p2 = findForCount 2 (allExcept p2 values)
+                               v' = head (allExcept p1 (allExcept p2 values))
+                           in TwoPairs p1 p2 v'
+  | isOnePair hand       = let p = findForCount 2 values
+                               vs = allExcept p values
+                           in OnePair p vs
+  | otherwise            = HighCard (sortBy compare values)
     where values = faceValues hand
-          countedValues = count values
+          allExcept v vs = filter (/= v) vs
+          findForCount n vs = v
+            where Just (v, _) = find (\(_,c) -> c == n) (count vs)
 
 isRoyalFlush :: [Card] -> Bool
 isRoyalFlush hand = isStraightFlush hand && (faceValue . highCard) hand == Ace
@@ -227,9 +197,9 @@ count (x:xs)
                   (count xs)
   | otherwise = (x, 1) : (count xs)
 
-filterRank :: Rank -> [((Rank, Rank), Integer)] -> [((Rank, Rank), Integer)]
-filterRank (Rank ord _) = 
-  filter (\((Rank ord1 _, Rank ord2 _), _) -> ord1 == ord || ord2 == ord)
+--filterRank :: Rank -> [((Rank, Rank), Integer)] -> [((Rank, Rank), Integer)]
+--filterRank (Rank ord _) = 
+--  filter (\((Rank ord1 _, Rank ord2 _), _) -> ord1 == ord || ord2 == ord)
 
 -- win h1 h2 evaluates to True if h1 is a better hand than h2, False otherwise
 win :: [Card] -> [Card] -> Bool
@@ -246,4 +216,5 @@ main = do
     print (take 5 ranks)
     print (count (suits deck))
     print (count (faceValues deck))
+    print (foldl (\n (r1, r2) -> if r1 > r2 then n + 1 else n) 0 ranks) 
     hClose handle
