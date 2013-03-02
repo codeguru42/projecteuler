ffa58724ad4b123487d7e7d13e4e19c0af51892d
codeguru <codeguru@users.sourceforge.net>
2013-03-02 03:25:46 -0700

    Problem 102: Point and Triangle data types

---
+++ problem102.hs
@@ -0,0 +1,14 @@
+-- This program is free software. It comes without any warranty, to
+-- the extent permitted by applicable law. You can redistribute it
+-- and/or modify it under the terms of the Do What The Fuck You Want
+-- To Public License, Version 2, as published by Sam Hocevar. See
+-- http://sam.zoy.org/wtfpl/COPYING for more details.
+
+-- Problem 102
+
+type Point = (Double, Double)
+
+data Triangle = Triangle Point Point Point
+
+--contains :: Triangle -> Point -> Boolean
+--contains t p = 
