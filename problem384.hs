86c4511d83dd03a4d8bf2801a546cd6fbc9c3d74
codeguru <codeguru@users.sourceforge.net>
2013-05-27 16:19:45 -0600

    Problem 384: Implement 'toBinaryString :: Integer -> String'

--- problem384.hs
+++ problem384.hs
@@ -6,20 +6,9 @@
 
 -- Problem 384
 
-a :: Int -> Int
-a n = a' (n / 2) (n `mod` 2)
-  where a' n b
-          | n == 0 = 0
-          | n == 1 = 0
-          | b == 1 && b' == 1 = 1 + a' n' b'
-          | otherwise a' n' b'
-            where b' = n `mod` 2
-                  n' = n / 2
-    
-b :: Int -> Int
-b n = (-1) ^ (a n)
+toBinaryString :: Integer -> String
+toBinaryString 0 = "0"
+toBinaryString 1 = "1"
+toBinaryString n = toBinaryString (n `div` 2) ++ toBinaryString (n `mod` 2)
 
-s :: Int -> Int
-s n = sum [b i | i <- [0..n]]
-
-main = print [(n, a n, b n, s n) | n -> [0..7]]
+main = print $ map toBinaryString [0..45]
