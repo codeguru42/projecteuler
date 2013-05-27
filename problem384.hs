f28ef8437988becb1853e592d72c5bb950a4cbe7
codeguru <codeguru@users.sourceforge.net>
2013-05-27 17:39:20 -0600

    Problem 384: Implement 'g :: Integer -> Integer -> Integer'

--- problem384.hs
+++ problem384.hs
@@ -6,6 +6,8 @@
 
 -- Problem 384
 
+import Data.List (elemIndex)
+
 toBinaryString :: Integer -> String
 toBinaryString 0 = "0"
 toBinaryString 1 = "1"
@@ -26,6 +28,12 @@ s n = sum [b i | i <- [0..n]]
 ss :: [Integer]
 ss = map s [1..]
 
+g :: Integer -> Integer -> Integer
+g t c = fromIntegral $ g' ss t c
+  where g' ss t 1 = let Just k = elemIndex t ss in k + 1
+        g' ss t c = k + 1 + g' (drop (k + 1) ss) t (c - 1)
+                    where Just k = elemIndex t ss
+
 fib :: Integer -> Integer
 fib 0 = 1
 fib 1 = 1
@@ -37,4 +45,7 @@ main = do
   print $ map a ns
   print $ map b ns
   print $ take 50 ss
+  print $ g 3 3
+  print $ g 4 2
+  print $ g 54321 12345
        where ns = [0..45]
