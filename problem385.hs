e5235dd248b1bda6959e6b2f5496b1fa32538a97
codeguru <codeguru@users.sourceforge.net>
2013-07-13 20:42:43 -0600

    Problem 385: Calculate the area of a triangle
    Create Vertex and Triangle data types and Shape type class.
    Implement 'distance :: Vertex -> Vertex -> Double'. Create an
    instance of the Shape type class for Triangle.

--- problem385.hs
+++ problem385.hs
@@ -5,3 +5,20 @@
 -- http://sam.zoy.org/wtfpl/COPYING for more details.
 
 -- Problem 385
+
+data Vertex = Vertex Double Double
+data Triangle = Triangle Vertex Vertex Vertex
+
+class Shape s where
+    area :: s -> Double
+
+instance Shape Triangle where
+    area (Triangle v1 v2 v3) = let a = distance v1 v2
+                                   b = distance v2 v3
+                                   c = distance v3 v1
+                                   s = (a + b + c) / 2
+                               in sqrt $ s * (s - a) * (s - b) * (s - c)
+
+distance :: Vertex -> Vertex -> Double
+distance (Vertex x1 y1) (Vertex x2 y2)
+    = sqrt $ (x2 - x1)^2 + (y2 - y1)^2
