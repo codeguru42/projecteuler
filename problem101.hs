6e9cd19f9253a03fe0f3d418396dfb49a404e92e
codeguru <codeguru@users.sourceforge.net>
2013-12-19 14:42:40 -0700

    Problem 101: Implement `fit`

--- problem101.hs
+++ problem101.hs
@@ -11,5 +11,13 @@ type Polynomial = [Integer]
 eval :: Polynomial -> Integer -> Integer
 eval p x = sum $ zipWith (*) p (map (x^) [0..])
 
-main = print (take 10 (map (eval gen) [1..]))
+fit :: Polynomial -> [Integer] -> Integer
+fit poly sequence = fst . head . filter (\(x,y)->x/=y) $ zip sequence gen
+    where gen = map (eval poly) [1..]
+
+main = do
+    print sequence
+    print $ fit poly sequence
     where gen = [1, -1, 1, -1, 1, -1, 1, -1, 1, -1, 1]
+          sequence = (take 11 (map (eval gen) [1..]))
+          poly = [1]
