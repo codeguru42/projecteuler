930bd237b26dfcec497d2830db058b8185da6c23
codeguru <codeguru@users.sourceforge.net>
2014-04-22 20:02:54 -0600

    Problem 203: Fix comment

--- problem203.hs
+++ problem203.hs
@@ -4,7 +4,7 @@
 -- To Public License, Version 2, as published by Sam Hocevar. See
 -- http://sam.zoy.org/wtfpl/COPYING for more details.
 
--- Problem 129
+-- Problem 203
 
 import Data.List (nub)
 import ProjectEuler
