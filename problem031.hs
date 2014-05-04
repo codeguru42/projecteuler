1337b45a706d8e632c575cebff8e104be33e88b7
codeguru <codeguru@users.sourceforge.net>
2014-05-03 20:02:49 -0600

    Problem 31: Dynamic programming solution

--- problem031.hs
+++ problem031.hs
@@ -13,6 +13,17 @@ countChange n (x:[]) = if n `mod` x == 0 then 1 else 0
 countChange n (x:xs) = countChange n xs + countChange (n - x) (x:xs) + maybePlusOne
     where maybePlusOne = if n == x then 1 else 0
 
-main = print $ countChange n denoms
+countChangeDP :: Int -> [Int] -> Int
+countChangeDP n xs = countChangeDP' !! (l-1) !! (n)
+    where l = length xs
+          countChangeDP' = [[cc i j | i <- [0..n]] | j <- [0..l-1]]
+          cc 0 _ = 0
+          cc i 0 = if i `mod` head xs == 0 then 1 else 0
+          cc i j = countChangeDP' !! (j - 1) !! i
+                 + countChangeDP' !! j !! max 0 (i - (xs !! j))
+                 + maybePlusOne
+            where maybePlusOne = if i == xs !! j then 1 else 0
+
+main = print $ countChangeDP n denoms
     where n = 200
           denoms = [1, 2, 5, 10, 20, 50, 100, 200]
