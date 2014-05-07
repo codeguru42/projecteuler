3bf774d53b0be356acc3a2c4d795e0aedbd27694
codeguru <codeguru@users.sourceforge.net>
2014-05-06 21:33:10 -0600

    Problem 81: Memoized solution

--- problem081.hs
+++ problem081.hs
@@ -15,19 +15,22 @@ toTriangle xss = [[t i j | i <- [0..j]] | j <- [0..m-1]]
           t i j = xss !! abs (i - j) !! i
 
 minSumPath :: [[Int]] -> Int -> Int -> Int
-minSumPath xss 0 0 = xss !! 0 !! 0
-minSumPath xss i 0 = xss !! i !! 0 + minSumPath xss (i-1) 0
-minSumPath xss 0 j = xss !! 0 !! j + minSumPath xss 0 (j-1)
-minSumPath xss i j = min (x + minSumPath xss (i-1)  j   )
-                         (x + minSumPath xss  i    (j-1))
-    where x = xss !! i !! j
-          max = length xss
+minSumPath xss i j = minSumPath' !! i !! j
+    where minSumPath'      = [[minSumPath'' i j | i <- [0..m]] | j <- [0..n]]
+          n = length xss
+          m = length $ xss !! 0
+          minSumPath'' 0 0 = xss !! 0 !! 0
+          minSumPath'' i 0 = xss !! i !! 0 + minSumPath' !! 0 !! (i-1)
+          minSumPath'' 0 j = xss !! 0 !! j + minSumPath' !! (j-1) !! 0
+          minSumPath'' i j = min (x + minSumPath' !! j !! (i-1))
+                                 (x + minSumPath' !! (j-1) !! i)
+            where x = xss !! i !! j
 
 main = do
-    contents <- readFile "input/matrix2.txt"
+    contents <- readFile "input/matrix.txt"
     let xss = map (read <$>) . map (splitOn ",") $ lines contents :: [[Int]]
     mapM_ print xss
     print $ length xss
     print . length $ last xss
     print $ minSumPath xss (n-1) (n-1)
-        where n = 5
+        where n = 80
