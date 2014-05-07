9b22a273872bd57485f5ee2a0271f5b274f0cca4
codeguru <codeguru@users.sourceforge.net>
2014-05-06 21:03:29 -0600

    Problem 81: Working solution

--- problem081.hs
+++ problem081.hs
@@ -14,7 +14,20 @@ toTriangle xss = [[t i j | i <- [0..j]] | j <- [0..m-1]]
     where m = length xss
           t i j = xss !! abs (i - j) !! i
 
+minSumPath :: [[Int]] -> Int -> Int -> Int
+minSumPath xss 0 0 = xss !! 0 !! 0
+minSumPath xss i 0 = xss !! i !! 0 + minSumPath xss (i-1) 0
+minSumPath xss 0 j = xss !! 0 !! j + minSumPath xss 0 (j-1)
+minSumPath xss i j = min (x + minSumPath xss (i-1)  j   )
+                         (x + minSumPath xss  i    (j-1))
+    where x = xss !! i !! j
+          max = length xss
+
 main = do
-    contents <- readFile "input/matrix.txt"
+    contents <- readFile "input/matrix2.txt"
     let xss = map (read <$>) . map (splitOn ",") $ lines contents :: [[Int]]
-    mapM_ print $ toTriangle xss
+    mapM_ print xss
+    print $ length xss
+    print . length $ last xss
+    print $ minSumPath xss (n-1) (n-1)
+        where n = 5
