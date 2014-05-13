abfd1389e372919f9e6d091597da01bbfd93bf95
codeguru <codeguru@users.sourceforge.net>
2014-05-13 09:01:07 -0600

    Problem 65: Compute continued fraction for e

--- problem065.hs
+++ problem065.hs
@@ -10,4 +10,9 @@ continuedFraction :: Double -> [Int]
 continuedFraction x = n : continuedFraction (1 / (x - fromIntegral n))
     where n = floor x
 
-main = print . take 25 . continuedFraction $ sqrt 2 
+eContinuedFraction :: [Int]
+eContinuedFraction = 2 : 1 : 2 : [e' n | n <- [3..]]
+    where e' n | n `mod` 3 == 2 = (eContinuedFraction !! (n - 3)) + 2
+               | otherwise = 1
+
+main = print . take 25 $ eContinuedFraction
