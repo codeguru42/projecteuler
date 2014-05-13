fc15616cc585fde24104086c798ba0c9e3627125
codeguru <codeguru@users.sourceforge.net>
2014-05-13 09:44:13 -0600

    Problem 65: Test case for `eContinuedFraction`

--- problem065.hs
+++ problem065.hs
@@ -6,6 +6,9 @@
 
 -- Problem 65
 
+import Test.HUnit.Base (Test(..), (~:), (~=?))
+import Test.HUnit.Text (runTestTT)
+
 continuedFraction :: Double -> [Int]
 continuedFraction x = n : continuedFraction (1 / (x - fromIntegral n))
     where n = floor x
@@ -14,5 +17,10 @@ eContinuedFraction :: [Int]
 eContinuedFraction = 2 : 1 : 2 : [e' n | n <- [3..]]
     where e' n | n `mod` 3 == 2 = (eContinuedFraction !! (n - 3)) + 2
                | otherwise = 1
+
+testEContinuedFraction :: Test
+testEContinuedFraction = "Test eContinuedFraction" ~: expected ~=? actual
+    where expected = [2, 1, 2, 1, 1, 4, 1, 1, 6, 1, 1, 8, 1, 1, 10, 1, 1, 12, 1, 1, 14, 1, 1, 16, 1]
+          actual   = take 25 eContinuedFraction
 
 main = print . take 25 $ eContinuedFraction
