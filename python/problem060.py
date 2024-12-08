a85018f155cc85da2ef6aac939fd24c674917cb0
Code-Apprentice <codeguru42@gmail.com>
2024-12-07 21:19:07 -0700

    Problem 60: Try adding one to known set

--- python/problem060.py
+++ python/problem060.py
@@ -1,20 +1,23 @@
-import itertools
-
 import project_euler
 
+is_prime = project_euler.sieve(400_000_000)
 
-def prime_pairs(n: int):
-    is_prime = project_euler.sieve(n*n)
-    ps = project_euler.primes(n)
-    for p1, p2 in itertools.combinations(ps, 2):
-        x = int(str(p1) + str(p2))
-        y = int(str(p2) + str(p1))
-        if is_prime[x] and is_prime[y]:
-            yield p1, p2
+
+def is_prime_pair(p1: int, p2: int) -> bool:
+    x = int(str(p1) + str(p2))
+    y = int(str(p2) + str(p1))
+    return is_prime[x] and is_prime[y]
+
 
 def main():
-    for x, y in prime_pairs(10_000):
-        print(x, y)
+    ps = project_euler.primes(200000)
+    four = [3, 7, 109, 673]
+    for p in ps:
+        if p > 673:
+            print(p)
+            if all(is_prime_pair(p, x) for x in four):
+                print(sum(four) + p)
+                break
 
 
 if __name__ == "__main__":
