414b5749917080ff170fd84fa658f26169563390
Code-Apprentice <codeguru42@gmail.com>
2024-12-07 18:21:35 -0700

    Problem 7: Implement Sieve of Eratosthenes

--- python/problem007.py
+++ python/problem007.py
@@ -1,5 +1,18 @@
+def sieve(n: int):
+    primes: list[int] = [True] * (n + 1)
+    primes[0] = False
+    primes[1] = False
+    for i in range(1, n):
+        if primes[i]:
+            for j in range(i * 2, n + 1, i):
+                primes[j] = False
+    return primes
+
 def main():
-    pass
+    primes = sieve(2_000_000)
+    for i, is_prime in enumerate(primes):
+        if is_prime:
+            print(i)
 
 
 if __name__ == "__main__":
