7caa2d37f19ab2a091dfd3c3c862a30496240647
Code-Apprentice <codeguru42@gmail.com>
2024-12-07 20:36:44 -0700

    Problem 60: Print prime pairs

--- python/problem060.py
+++ python/problem060.py
@@ -1,5 +1,20 @@
+import itertools
+
+import project_euler
+
+
+def prime_pairs(n: int):
+    is_prime = project_euler.sieve(n*n)
+    ps = project_euler.primes(n)
+    for p1, p2 in itertools.combinations(ps, 2):
+        x = int(str(p1) + str(p2))
+        y = int(str(p2) + str(p1))
+        if is_prime[x] and is_prime[y]:
+            yield p1, p2
+
 def main():
-    pass
+    for x, y in prime_pairs(10_000):
+        print(x, y)
 
 
 if __name__ == "__main__":
