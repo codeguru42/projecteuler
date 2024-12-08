2b36180143f80401f164e0c81fc8972242cf64ed
Code-Apprentice <codeguru42@gmail.com>
2024-12-07 18:23:18 -0700

    Problem 7: Print 10,001st prime number

--- python/problem007.py
+++ python/problem007.py
@@ -10,9 +10,13 @@ def sieve(n: int):
 
 def main():
     primes = sieve(2_000_000)
+    count = 0
     for i, is_prime in enumerate(primes):
         if is_prime:
+            count += 1
+        if count == 10_001:
             print(i)
+            break
 
 
 if __name__ == "__main__":
