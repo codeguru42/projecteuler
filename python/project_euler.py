13441f0e9a7f08cc709349ea2490aa91374be1b3
Code-Apprentice <codeguru42@gmail.com>
2024-12-08 19:49:09 -0700

    grouper()

--- python/project_euler.py
+++ python/project_euler.py
@@ -1,3 +1,6 @@
+import itertools
+
+
 def sieve(n: int):
     ps: list[int] = [True] * (n + 1)
     ps[0] = False
@@ -14,3 +17,19 @@ def primes(n: int):
     for i, is_prime in enumerate(ps):
         if is_prime:
             yield i
+            
+def grouper(iterable, n, *, incomplete='fill', fillvalue=None):
+    "Collect data into non-overlapping fixed-length chunks or blocks."
+    # grouper('ABCDEFG', 3, fillvalue='x') → ABC DEF Gxx
+    # grouper('ABCDEFG', 3, incomplete='strict') → ABC DEF ValueError
+    # grouper('ABCDEFG', 3, incomplete='ignore') → ABC DEF
+    iterators = [iter(iterable)] * n
+    match incomplete:
+        case 'fill':
+            return itertools.zip_longest(*iterators, fillvalue=fillvalue)
+        case 'strict':
+            return zip(*iterators, strict=True)
+        case 'ignore':
+            return zip(*iterators)
+        case _:
+            raise ValueError('Expected fill, strict, or ignore')
