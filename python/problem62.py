f925bae2a6af398252c866456cfe0036de926b3a
Code-Apprentice <codeguru42@gmail.com>
2024-12-08 23:21:29 -0700

    Problem 62: Print some cubes

--- python/problem62.py
+++ python/problem62.py
@@ -1,5 +1,14 @@
+def cubes(start=0):
+    x = start
+    while True:
+        yield x*x*x
+        x += 1
+
 def main():
-    pass
+    for x in cubes():
+        if x > 1_000_000_000:
+            break
+        print(x)
 
 
 if __name__ == '__main__':
